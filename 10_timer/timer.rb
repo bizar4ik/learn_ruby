cb9bba6ae8ea0ea46891189a04d1e48f87ab37f2
bizar4ik <unknown.b3@inbox.ru>
2013-01-17 11:02:42 +0200

    10 done

---
+++ 10_timer/timer.rb
@@ -0,0 +1,33 @@
+class Timer
+
+  def initialize()
+    @sec=0
+  end
+
+
+  def seconds
+    @sec
+  end
+
+  def seconds=(sec)
+    @sec=sec
+
+  end
+
+  def time_to_str(temp)
+     if temp<9
+       "0#{temp}"
+     else "#{temp}"
+     end
+  end
+
+  def time_string
+    hour=@sec/3600
+    min=(@sec%3600)/60
+    seconds=@sec-hour*3600-min*60
+
+time_to_str(hour)+":"+time_to_str(min)+":"+time_to_str(seconds)
+
+  end
+
+end
