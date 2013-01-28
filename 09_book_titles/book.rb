cbbf0c2e1562bae25df70e5004ed4707507f306f
bizar4ik <unknown.b3@inbox.ru>
2013-01-28 18:21:22 +0200

    09 corected

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -5,22 +5,18 @@ class Book
   end
 
   def title=(name)
-
-    res=[]
-    litle_words=["and","the","a","an","in","of"]
-    words=name.split(" ")
-    n=words.size
+    res = []
+    litle_words = ["and", "the", "a", "an", "in", "of"]
+    words = name.split(" ")
+    n = words.size
 
    res.push words[0].capitalize
     words[1..n].each do |word|
-
       if litle_words.include? word
         res.push word
       else  res.push word.capitalize
       end
-
     end
-@title=res.join(" ")
+    @title = res.join(" ")
   end
-
 end
