5c229b46120436918a05cfc0e80f7021b3c1d63d
bizar4ik <unknown.b3@inbox.ru>
2013-01-28 18:14:15 +0200

    05 corected

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,19 +1,18 @@
 def reverser
-words=yield
-result=[]
+  words = yield
+  result = []
 	words.split.map do |word|
 		 result.push word.reverse	 
 	end
-result.join(" ")
+  result.join(" ")
 end
 
-def adder(num=1)
-	yield+num
+def adder(num = 1)
+	yield + num
 end
 
-def repeater(num=1)
-	num.times do
-	yield
+def repeater(num = 1)
+	num.times do yield
 end
 
 end
