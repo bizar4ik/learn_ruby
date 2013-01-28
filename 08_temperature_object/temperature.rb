c6455f0c7f2ef3660c4586cbcd76415493150443
bizar4ik <unknown.b3@inbox.ru>
2013-01-28 18:20:26 +0200

    08 corected

--- 08_temperature_object/temperature.rb
+++ 08_temperature_object/temperature.rb
@@ -1,42 +1,38 @@
 class Temperature
 
 
-  def initialize(hash={})
+  def initialize(hash = {})
     if hash.include?(:f)
-      @f_temp=hash[:f]
-      @c_temp=ftoc(hash[:f])
-
+      @f_temp = hash[:f]
+      @c_temp = ftoc(hash[:f])
     elsif hash.include?(:c)
-      @c_temp=hash[:c]
-      @f_temp=ctof(hash[:c])
-
+      @c_temp = hash[:c]
+      @f_temp = ctof(hash[:c])
     end
   end
 
   def Temperature.from_celsius(temp)
-    Temperature.new(:c=>temp)
+    Temperature.new(:c => temp)
   end
 
   def Temperature.from_fahrenheit(temp)
-    Temperature.new(:f=>temp)
+    Temperature.new(:f => temp)
   end
 
-
     def in_celsius
-    @c_temp
+      @c_temp
     end
 
     def in_fahrenheit
-     @f_temp
+      @f_temp
     end
 
-
     def ftoc(temp)
-      (temp-32)/9*5.0
+      (temp-32) / 9 * 5.0
     end
 
     def ctof(temp)
-      (temp*9/5.0)+32
+      (temp * 9 / 5.0) + 32
     end
 end
 
@@ -44,13 +40,13 @@ end
 
 class Celsius <Temperature
 	def initialize(temp)
-	super(:c=>temp)
+	super(:c => temp)
 	end
 end
 
 class Fahrenheit <Temperature
 	def initialize(temp)
-	super(:f=>temp)
+	super(:f => temp)
 	end
 end
 
